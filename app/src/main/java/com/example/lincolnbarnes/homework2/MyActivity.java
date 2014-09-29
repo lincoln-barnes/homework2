575fdef0616ba317be5508b99c1975d28f64a698
lincoln-barnes <lincoln.barnes@mavs.uta.edu>
2014-09-29 09:25:56 -0500

    My commit message

--- app/src/main/java/com/example/lincolnbarnes/homework2/MyActivity.java
+++ app/src/main/java/com/example/lincolnbarnes/homework2/MyActivity.java
@@ -1,36 +1,56 @@
 package com.example.lincolnbarnes.homework2;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MyActivity extends Activity {
 
+    ArrayList<String> tags;
+    EditText tv;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        tags = new ArrayList<String>();
+        tv = (EditText) findViewById(R.id.textView);
+        final ListView listView = (ListView) findViewById(R.id.listView1);
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, tags);
+
+        listView.setAdapter(adapter);
+
     }
 
+    /*public void onSaveClick(View v) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick a state")
+                .setItems(statesArray, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (!states.contains(statesArray[which])) {
+                            states.add(statesArray[which]);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+        builder.create().show();
+
+    }*/
 }
